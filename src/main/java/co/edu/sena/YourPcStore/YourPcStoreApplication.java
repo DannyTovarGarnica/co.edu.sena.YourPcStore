f7ef3a4d385af51a6e10a57b43420d29a6b86ccc
Aitcheffoh <hecfpasaje@misena.edu.co>
2022-04-03 02:53:00 -0500

    feat initial commit

---
+++ src/main/java/co/edu/sena/YourPcStore/YourPcStoreApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.YourPcStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPcStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourPcStoreApplication.class, args);
+	}
+
+}
